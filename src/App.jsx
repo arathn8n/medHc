1ba1ad6b232dbc4887d74a0579a28c5107166fe7
arath abello <arathabello60@gmail.com>
2026-05-02 11:34:39 -0400

    terminando estilos loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react"
+import Loader from "./components/loader/Loader";
+import Home from "./components/home/Home";
 
 
 function App() {
  
   const [loader, setLoader] = useState(true)
 
+  // Se ejecuta una sola vez al montar el componente (por el array vacío []).
   useEffect(()=>{
    const time = setTimeout(() => {
       setLoader(false)
     }, 3000);
-
+    
     return ()=> clearTimeout(time)
   }, [])
 
   return (
     <>
-      
+      {loader ? <Loader/> : <Home/>}
     </>
   )
 }
