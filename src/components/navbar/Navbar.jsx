2c0a85a7ba914d0145e51a62f125e176752e159e
arath abello <arathabello60@gmail.com>
2026-05-03 09:43:39 -0400

    arreglando estilos y empezando con navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,23 +14,23 @@ const Navbar = () => {
                         <span> info@gmail.com</span>
                     </div>
 
-                    <ul>
-                        <li className='me-3'>
+                    <ul className='ulList'>
+                        <li className='me-3 ulListLi'>
                             <a href="#">
                                 <i className="fa-brands fa-facebook-f"></i>
                             </a>
                         </li>
-                        <li className='me-3'>
+                        <li className='me-3 ulListLi'>
                             <a href="#">
                                 <i className="fa-brands fa-x-twitter"></i>
                             </a>
                         </li>
-                        <li className='me-3'>
+                        <li className='me-3 ulListLi'>
                             <a href="#">
                                 <i className="fa-brands fa-linkedin-in"></i>
                             </a>
                         </li>
-                        <li>
+                        <li className='ulListLi'>
                             <a href="#">
                                 <i className="fa-brands fa-instagram"></i>
                             </a>
@@ -61,6 +61,29 @@ const Navbar = () => {
                     </figure>
                 </div>
             </div>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
